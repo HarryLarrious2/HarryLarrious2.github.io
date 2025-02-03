8b20057557e52f6c715eff248a3820f1ebcc2c9f
HarryLarrious <harrylarrious90@gmail.com>
2025-02-03 17:29:46 -0500

    Initial commit

---
+++ externalHandler.js
@@ -0,0 +1,16 @@
+window.addEventListener('message', function(event) {
+    // Allow all origins
+    event.source.postMessage('Message received', '*');
+
+    // Process the received data
+    var receivedData = event.data;
+    
+    // Display the message in the list
+    var messageList = document.getElementById('messageList');
+    var messageItem = document.createElement('p');
+    messageItem.textContent = 'From ' + event.origin + ': ' + JSON.stringify(receivedData);
+    messageList.appendChild(messageItem);
+
+    // Scroll to the bottom of the list
+    messageList.scrollTop = messageList.scrollHeight;
+}, false);
